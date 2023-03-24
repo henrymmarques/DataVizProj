b3689cf220df87b10cfae05baf34ebfac5fb52f1
hmpms-iscte <hmpms@iscte-iul.pt>
2023-03-24 10:06:31 +0000

    save

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from dash import dcc
 from dash import html
 import plotly.graph_objs as go
 import pandas as pd
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Load the data
-data = pd.read_csv('World Energy Consumption.csv')
-continents = pd.read_csv('continents2.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/henrymmarques/DataVizProj/master/World%20Energy%20Consumption.csv')
+continents = pd.read_csv('https://raw.githubusercontent.com/henrymmarques/DataVizProj/master/continents2.csv')
+
+#Preprocess the data
 continents = continents[['name', 'alpha-3', 'region', 'sub-region']]
 regions = data.iso_code.replace(continents.set_index('alpha-3')['region'])
 data['region']=regions
@@ -14,6 +18,7 @@ data = data[data['iso_code'].notna()]
 sub_regions = data.iso_code.replace(continents.set_index('alpha-3')['sub-region'])
 data['sub_region']=sub_regions
 
+#df for oil_consumption plots
 oil_consumption_df=data[data['oil_consumption'].notna()][['oil_consumption', 'year', 'iso_code', 'country', 'region', 'sub_region' ]]
 
 # Filter the data
@@ -37,15 +42,15 @@ oil_consumption_region = oil_consumption_df.loc[oil_consumption_df['region']!='O
 oil_consumption_region = oil_consumption_region.reset_index()[['region', 'year', 'oil_consumption']]
 
 # Create an empty graph object
-fig_bar = go.Figure()
-fig_bar2 = go.Figure()
+fig_oil_consu_slider = go.Figure()
+fig_oil_consu = go.Figure()
 
 # Loop through each unique year in the dataset and create a trace for each year
 for year in oil_consumption_region['year'].unique():
     # Filter the dataframe to only include data for the current year
     data_for_year = oil_consumption_region[oil_consumption_region['year'] == year]
     # Create a trace for the current year
-    fig_bar.add_trace(dict(type='bar',
+    fig_oil_consu_slider.add_trace(dict(type='bar',
                      x=data_for_year['region'],
                      y=data_for_year['oil_consumption'],
                      showlegend=False,
@@ -57,7 +62,7 @@ for year in oil_consumption_region['year'].unique():
                )
 
 # First seen trace
-fig_bar.data[0].visible = True
+fig_oil_consu_slider.data[0].visible = True
 
 # Create a slider step for each year
 steps = []
@@ -88,8 +93,8 @@ layout = dict(title=dict(text='Oil consumption by continent between 1965 and 201
             )
 
 # Add the traces to the figure
-fig_bar.update_layout(layout)
-fig_bar2.add_trace(trace_world)
+fig_oil_consu_slider.update_layout(layout)
+fig_oil_consu.add_trace(trace_world)
 
 # Create the app
 app = dash.Dash(__name__)
@@ -98,9 +103,9 @@ server = app.server
 # Define the layout
 app.layout = html.Div([
     html.H1(children='Oil Consumption'),
-    dcc.Graph(id='oil_consumption', figure=fig_bar),
+    dcc.Graph(id='oil_consumption', figure=fig_oil_consu_slider),
     html.Br(),
-    dcc.Graph(id='oil_consumption 2 ', figure=fig_bar2),
+    dcc.Graph(id='oil_consumption 2 ', figure=fig_oil_consu),
 ])
 
 if __name__ == '__main__':
